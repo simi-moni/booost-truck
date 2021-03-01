edb42f4bfade8746f73e31d642a7d2157a2c3d63
Simona <monisimi99@gmail.com>
2021-03-01 21:37:57 +0200

    fix: fixing timeline

--- src/app/custom/Animation.js
+++ src/app/custom/Animation.js
@@ -1,37 +1,9 @@
+import gsap from 'gsap/all';
 import selectors from './selectors';
 
 export default class Animation {
-    constructor(tl) {
-        this._tl = tl;
-    }
-
-    start() {
-        const self = this;
-        selectors.playBtn.addEventListener('click', function () {
-            if (self._tl.isActive()) {
-                self._tl.progress(0);
-            }
-            self.playAnimation(self._tl).play();
-        });
-
-        selectors.truckBtn.addEventListener('click', function () {
-            if (self._tl.isActive()) {
-                self._tl.progress(0);
-            }
-            self.playAnimation(self._tl).play();
-        });
-
-        selectors.reverseBtn.addEventListener('click', function () {
-            self.playAnimation(self._tl).reverse();
-        });
-
-        selectors.pauseBtn.addEventListener('click', function () {
-            self.playAnimation(self._tl).pause();
-        });
-    }
-
-    playAnimation(tl) {
-        return tl
+    constructor() {
+        this._tl = gsap.timeline({ paused: true })
             .to(selectors.list, { y: -100, id: "listUp" })
             .to(selectors.list, { y: 10, ease: "power3.out,", id: "listDown" })
             .to(selectors.listItems[0], { y: 30, opacity: 0, id: 'listItem0' }, 'listItems')
@@ -51,5 +23,30 @@ export default class Animation {
             .to(selectors.frontWheelsBack, { opacity: 1, id: 'frontWheelBack' }, 'frontWheels')
             .to(selectors.truck, { ease: 'back.in(3)', x: 600, opacity: 0, duration: 2, id: 'truckMovement' })
             .to(selectors.shippedLabel, { opacity: 1, id: 'shippedLabel' })
+    }
+
+    start() {
+        const self = this;
+        selectors.playBtn.addEventListener('click', function () {
+            if (self._tl.isActive()) {
+                self._tl.progress(0);
+            }
+            self._tl.play();
+        });
+
+        selectors.truckBtn.addEventListener('click', function () {
+            if (self._tl.isActive()) {
+                self._tl.progress(0);
+            }
+            self._tl.play();
+        });
+
+        selectors.reverseBtn.addEventListener('click', function () {
+            self._tl.reverse();
+        });
+
+        selectors.pauseBtn.addEventListener('click', function () {
+            self._tl.pause();
+        });
     }
 }
