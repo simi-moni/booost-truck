7891fc042cb481837413856b60b742373af84534
Simona <monisimi99@gmail.com>
2021-03-01 19:53:11 +0200

    feat: added animation

---
+++ src/app/custom/Animation.js
@@ -0,0 +1,55 @@
+import selectors from './selectors';
+
+export default class Animation {
+    constructor(tl) {
+        this._tl = tl;
+    }
+
+    start() {
+        const self = this;
+        selectors.playBtn.addEventListener('click', function () {
+            if (self._tl.isActive()) {
+                self._tl.progress(0);
+            }
+            self.playAnimation(self._tl).play();
+        });
+
+        selectors.truckBtn.addEventListener('click', function () {
+            if (self._tl.isActive()) {
+                self._tl.progress(0);
+            }
+            self.playAnimation(self._tl).play();
+        });
+
+        selectors.reverseBtn.addEventListener('click', function () {
+            self.playAnimation(self._tl).reverse();
+        });
+
+        selectors.pauseBtn.addEventListener('click', function () {
+            self.playAnimation(self._tl).pause();
+        });
+    }
+
+    playAnimation(tl) {
+        return tl
+            .to(selectors.list, { y: -100, id: "listUp" })
+            .to(selectors.list, { y: 10, ease: "power3.out,", id: "listDown" })
+            .to(selectors.listItems[0], { y: 30, opacity: 0, id: 'listItem0' }, 'listItems')
+            .to(selectors.listItems[1], { y: 30, opacity: 0, id: 'listItem1' }, 'listItems')
+            .to(selectors.listItems[2], { y: 30, opacity: 0, id: 'listItem2' }, 'listItems')
+            .to(selectors.truckBtnBg, { scale: 1.3, transformOrigin: 'center', id: "truckBtnScaleUp" })
+            .to(selectors.truckBtnBg, { scale: 1, transformOrigin: 'center', id: "truckBtnScaleDown" })
+            .to(selectors.container, { opacity: 1, id: "container" }, 'container')
+            .to(selectors.containerParts, { opacity: 1, id: "containerParts" }, 'container')
+            .to(selectors.backWheel1, { opacity: 1, id: 'backWheel1' }, 'backWheels')
+            .to(selectors.backWheelBack1, { opacity: 1, id: 'backWheelBack1', }, 'backWheels')
+            .to(selectors.backWheel2, { opacity: 1, id: 'backWheel2' }, 'backWheels')
+            .to(selectors.backWheelBack2, { opacity: 1, id: 'backWheelBack2', }, 'backWheels')
+            .to(selectors.frontGroup, { opacity: 1, id: 'frontGroup' }, 'frontWheels')
+            .to(selectors.frontWheel1, { opacity: 1, id: 'frontWheel1', }, 'frontWheels')
+            .to(selectors.frontWheel2, { opacity: 1, id: 'frontWheel2', }, 'frontWheels')
+            .to(selectors.frontWheelsBack, { opacity: 1, id: 'frontWheelBack' }, 'frontWheels')
+            .to(selectors.truck, { ease: 'back.in(3)', x: 600, opacity: 0, duration: 2, id: 'truckMovement' })
+            .to(selectors.shippedLabel, { opacity: 1, id: 'shippedLabel' })
+    }
+}
